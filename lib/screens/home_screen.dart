83595d1e39537366723feb992d450c44d2ee23e7
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-11-04 04:42:58 +0500

    updated work

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,9 +48,9 @@ class HomeScreen extends StatelessWidget{
         ),
          Row(
           children:[
-           const SizedBox(width:15),
+           const SizedBox(width:5),
             Container(
-              width: 130,
+              width: 150,
               height:190,
                decoration: BoxDecoration(
               color:const Color(0xFFA6A6A6),
@@ -90,23 +90,71 @@ class HomeScreen extends StatelessWidget{
               padding: EdgeInsets.only(top:10.0,left:10.0),
               child:Align(
                 alignment: Alignment.centerLeft,
-              child:Text("using Figma",style:TextStyle(fontSize:14,color:Color.fromARGB(255, 121, 117, 117),fontWeight:FontWeight.bold)),
+              child:Text("using Figma",style:TextStyle(fontSize:14,color:Color.fromRGBO(255, 255, 255, 0.58),fontWeight:FontWeight.bold)),
+              ),
+            ),
+              const Padding(
+              padding: EdgeInsets.only(top:0.0,left:10.0),
+              child:Align(
+                alignment: Alignment.centerLeft,
+              child:Text("and other tools",style:TextStyle(fontSize:14,color: Color.fromRGBO(255, 255, 255, 0.58),fontWeight:FontWeight.bold)),
               ),
             ),
           ],
       ),
             ),
-             const SizedBox(width:15),
+             const SizedBox(width:10),
               Container(
-              width: 130,
+              width: 150,
               height:190,
                decoration: BoxDecoration(
               color:const Color(0xFFA6A6A6),
         borderRadius: BorderRadius.circular(26), 
       ),
-      child:const Column(
+      child: Column(
+
           children:[
-            Text("2 hours Ago",style:TextStyle(fontSize:14,color:Colors.white))
+              Padding(
+      padding: const EdgeInsets.only(top: 16.0,left:90.0), 
+            child:Image.asset('assets/images/camera_icon.png',width:20,height:20),
+              ),
+            const Padding(
+      padding:  EdgeInsets.only(left:10.0), 
+            child: Align(
+        alignment: Alignment.centerLeft,
+         child:Text("4 hours Ago",style:TextStyle(fontSize:14,color:Colors.white))
+            ),
+            ),
+              const Padding(
+      padding:  EdgeInsets.only(top:10.0,left:10.0), 
+            child: Align(
+        alignment: Alignment.centerLeft,
+   
+         child:Text("Capture sun",style:TextStyle(fontSize:20,color:Colors.white,fontWeight:FontWeight.bold)),
+            ),
+            ),
+              const Padding(
+      padding:  EdgeInsets.only(left:10.0), 
+            child: Align(
+        alignment: Alignment.centerLeft,
+   
+         child:Text("Rise Shots",style:TextStyle(fontSize:20,color:Colors.white,fontWeight:FontWeight.bold)),
+            ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top:10.0,left:10.0),
+              child:Align(
+                alignment: Alignment.centerLeft,
+              child:Text("complete GuruShot",style:TextStyle(fontSize:14,color:Color.fromRGBO(255, 255, 255, 0.58),fontWeight:FontWeight.bold)),
+              ),
+            ),
+              const Padding(
+              padding: EdgeInsets.only(top:0.0,left:10.0),
+              child:Align(
+                alignment: Alignment.centerLeft,
+              child:Text("and other tools",style:TextStyle(fontSize:14,color: Color.fromRGBO(255, 255, 255, 0.58),fontWeight:FontWeight.bold)),
+              ),
+            ),
           ],
       ),
             ),
