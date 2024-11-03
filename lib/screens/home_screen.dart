2cc6691027a442740f1a82a5c80daf4240f5fc1b
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-11-03 06:04:30 +0500

    updated work

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,10 +8,12 @@ class HomeScreen extends StatelessWidget{
       backgroundColor:AppConstant.appBackgroundColor,
       body:Container(
         padding: const EdgeInsets.only(top: 50),
-        child: const Row(
+        child: Column(
+          children:[
+        const Row(
            
            children:[
-             SizedBox(width: 10),
+             SizedBox(width: 15),
              Text("Welcome to Notes",style:TextStyle(fontWeight: FontWeight.bold,fontSize:28)),
               SizedBox(width: 20),
              CircleAvatar(
@@ -21,6 +23,56 @@ class HomeScreen extends StatelessWidget{
             ),
         ],
         ),
+        const Row(
+           
+           children:[
+             SizedBox(width: 15),
+
+             Text("Have a great Day",style:TextStyle(fontSize:24)),
+           
+        ],
+        ),
+        const Row(
+          children: [
+            SizedBox(height: 20,)
+          ],
+        ),
+          const  Row(
+           
+           children:[
+             SizedBox(width: 10),
+
+             Text("My Priority Task ",style:TextStyle(fontSize:24)),
+           
+        ],
+        ),
+         Row(
+          children:[
+           const SizedBox(width:15),
+            Container(
+              width: 150,
+              height:190,
+               decoration: BoxDecoration(
+              color:const Color(0xFFA6A6A6),
+        borderRadius: BorderRadius.circular(26), 
+      ),
+            ),
+             const SizedBox(width:15),
+              Container(
+              width: 150,
+              height:190,
+               decoration: BoxDecoration(
+              color:const Color(0xFFA6A6A6),
+        borderRadius: BorderRadius.circular(26), 
+      ),
+            ),
+          ],
+          
+        ),
+          ],
+          
+        ),
+      
       ),
      
         
